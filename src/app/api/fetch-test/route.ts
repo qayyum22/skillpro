5a448ee5c37c58fce094223741150ea03f380929
ailabs22 <ailabs.2208@gmail.com>
2025-02-19 21:01:29 +0530

    Update route.ts

--- src/app/api/fetch-test/route.ts
+++ src/app/api/fetch-test/route.ts
@@ -1,18 +1,20 @@
-import clientPromise from "@/lib/mongodb";
+// import clientPromise from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
     const { input } = await req.json();
-    const client = await clientPromise;
-    if (!client) {
-      throw new Error("Failed to connect to the database");
-    }
-    const db = client.db("testprephaven");
-    // Save or update user in MongoDB for OAuth providers
-    const writingTest = await db
-      .collection("writingtest")
-      .findOne({ "tasks2.test_id": "02" });
+    // const client = await clientPromise;
+    // if (!client) {
+    //   throw new Error("Failed to connect to the database");
+    // }
+    // const db = client.db("testprephaven");
+    // // Save or update user in MongoDB for OAuth providers
+    // const writingTest = await db
+    //   .collection("writingtest")
+    //   .findOne({ "tasks2.test_id": "02" });
+
+    const writingTest = "hello";
 
     return NextResponse.json({ response: writingTest + input }, { status: 200 });
   } catch (error) {
